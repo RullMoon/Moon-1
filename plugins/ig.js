690f4a9c890dfd399cc1ec05fbdfcfc8e3045f17
RullMoonMC <107551141+RullMoonMC@users.noreply.github.com>
2022-06-16 05:35:34 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,7 +1,7 @@
 
 const { instagramdl } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/BmjK1KOD_UG/?utm_medium=copy_link`
+  if (!args[0]) throw `*Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/LKjB2KOD_UG/?utm_medium=copy_link`
   if (!args[0].match(/https:\/\/www.instagram.com\/(p|reel|tv)/gi)) throw `*Link salah! Perintah ini untuk mengunduh postingan ig/reel/tv, bukan untuk highlight/story!*\n\ncontoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
 
 instagramdl(args[0]).then(async res => {
